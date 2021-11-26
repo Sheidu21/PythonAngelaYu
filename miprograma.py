2f962ddec873d94e81c6daa6d6e8d61087779022
Sheidu21 <gregor.tamez@hotmail.com>
2021-11-25 18:43:55 -0600

    Update miprograma.py

--- miprograma.py
+++ miprograma.py
@@ -1,1 +1,8 @@
 # mi primer programa de Python
+
+# import complex math module
+import cmath
+# nombre = input("¿Cómo se llama? ")
+a = input("Escrfibe el coeficiente quadratico ")
+b = input("Escrfibe el coeficiente lineal")
+c = input("Escrfibe el coeficiente constante ")
